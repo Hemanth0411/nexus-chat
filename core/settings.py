aaf277869e0608f0e0d0e365bb7b33a959396cf2
Hemanth0411 <hemanthreddyannem@gmail.com>
2025-09-06 19:49:36 +0530

    feat: Add auth token endpoint for user login

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'user',
     'chatbot',
-    
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
--- user/views.py
+++ user/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
-from .serializers import UserSerializer
+from .serializers import UserSerializer, AuthTokenSerializer
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system."""
     serializer_class = UserSerializer
 
+class CreateTokenView(ObtainAuthToken):
+    serializer_class = AuthTokenSerializer
